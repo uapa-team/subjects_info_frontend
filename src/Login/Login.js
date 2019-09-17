091f4e4a0efc17b1a4cdc21f8ec13389d8059477
Laura Molina <ldmolinav@unal.edu.co>
2019-09-17 13:16:33 -0500

    first commit

---
+++ src/Login/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import LoginForm from './LoginForm.js';
+
+class Login extends React.Component {
+
+  render() {
+      return(
+        <LoginForm></LoginForm>
+      );
+  }
+}
+
+
+export default Login;
